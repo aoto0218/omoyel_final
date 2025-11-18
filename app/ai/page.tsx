2c98d4b58a9b34ab2533c8d9279f0d8635cbb1ed
aoto0218 <ktc24a31g0010@edu.kyoto-tech.ac.jp>
2025-11-18 14:04:12 +0900

    再度環境構築

--- app/ai/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-    return <h1>Hello im ai</h1>
-  }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import HeaderComponents from "./components/Header/page";
-import { FooterComponents } from "./components/Footer/page";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,9 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <HeaderComponents />
         {children}
-        <FooterComponents />
       </body>
     </html>
   );
