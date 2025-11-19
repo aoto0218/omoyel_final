1e70e4b566c08929c39a3e5c7ba7e9b7839b6b45
aoto0218 <ktc24a31g0010@edu.kyoto-tech.ac.jp>
2025-11-19 15:06:50 +0900

    ヘッダー部、フィルター部改善前修正

--- app/salon/[id]/page.tsx
+++ app/salon/[id]/page.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 import { useParams, useRouter } from 'next/navigation';
 import { ArrowLeft } from 'lucide-react';
 import { SalonImage } from '@/components/SalonImage';
-import { MOCK_SALONS } from '@/constants/data';
+import { MOCK_SALONS } from '@/constants/salondata';
 
 export default function SalonDetailPage() {
     const params = useParams();
