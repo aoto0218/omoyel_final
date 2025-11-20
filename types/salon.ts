c908c59c383791336e52a5d9d01fc4e66cbbfc54
aoto0218 <ktc24a31g0010@edu.kyoto-tech.ac.jp>
2025-11-20 14:57:47 +0900

    Supabaseデータ取得機能実装

--- types/salon.ts
+++ types/salon.ts
@@ -1,26 +1,52 @@
+export interface SalonDB {
+    id: number;
+    name: string;
+    location: string;
+    images: string;
+    address: string;
+    name_kana: string;
+    tags: string[];
+    image_type: string;
+    visit_schedule: string;
+    duration: string;
+    flow: string;
+    guide_staff: string;
+    contact: string;
+    instagram: string;
+    meeting_place: string;
+    staff_gender_ratio: string;
+    staff_age_group: string;
+    atmosphere: string;
+    customer_gender_ratio: string;
+    customer_age_group: string;
+    international_customer_frequency: string;
+}
+
 export interface Salon {
     id: number;
     name: string;
     location: string;
-    address?:string;
-    nameKana?:string;
-    images?:{image1:string,image2:string};
+    images?: {
+        image1?: string;
+        image2?: string;
+    };
+    address: string;
+    nameKana: string;
     tags: string[];
-    imageType: 'canvas' | 'shell';
-    visitSchedule?: string;
-    duration?: string;
-    flow?:string;
-    guideStaff?:string;
-    contact?:string;
-    instagram?: string;
-    meetingPlace?: string;
-    staffGenderRatio?: string;
-    staffAgeGroup?: string;
-    atmosphere?: string;
-    customerGenderRatio?: string;
-    customerAgeGroup?: string;
-    internationalCustomerFrequency?: string;
-    featured?: boolean;
+    imageType: string;
+    visitSchedule: string;
+    duration: string;
+    flow: string;
+    guideStaff: string;
+    contact?: string;
+    instagram: string;
+    meetingPlace: string;
+    staffGenderRatio: string;
+    staffAgeGroup: string;
+    atmosphere: string;
+    customerGenderRatio: string;
+    customerAgeGroup: string;
+    internationalCustomerFrequency: string;
 }
 
 export interface DropdownProps {
